d05649976ee49643730453bb59fdaf6b2fc0808d
iihossein <hosseinnazarigit@gmail.com>
2023-02-10 01:20:19 +0330

    Category Filter Added to Project

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -15,7 +15,9 @@
         <div class="relative lg:inline-flex bg-gray-100 rounded-xl w-30 text-left">
 
             <div x-data="{ show: false }" @click.away="show = false">
-                <button @click="(show = ! show)" class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-30 flex lg:inline-flex">Categories
+                <button @click="(show = ! show)"
+                    class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-30 flex lg:inline-flex">
+                    {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
                     <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
                         height="22" viewBox="0 0 22 22">
                         <g fill="none" fill-rule="evenodd">
@@ -29,33 +31,16 @@
                 </button>
                 <div x-show="show" class="py-0 absolute bg-gray-100 w-full mt-2 rounded-xl z-50"
                     style="display: none;">
-                    <a href="3"
-                        class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">One</a>
+
+                    <a href="/"
+                        class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">All</a>
+
+                    @foreach ($categories as $category)
+                        <a href="/categories/{{ $category->slug }}"
+                            class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white {{ isset($currentCategory) && $currentCategory->id === $category->id ? 'bg-blue-500 text-white' : '' }}">{{ ucwords($category->name) }}</a>
+                    @endforeach
                 </div>
             </div>
-
-
-
-
-
-            {{--
-            <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
-                @foreach ($categories as $category)
-                <option value="{{ $category->slug }}">{{ $category->name }}</option>
-
-                @endforeach
-            </select>
-
-            <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
-                height="22" viewBox="0 0 22 22">
-                <g fill="none" fill-rule="evenodd">
-                    <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                    </path>
-                    <path fill="#222"
-                        d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
-                    </path>
-                </g>
-            </svg> --}}
         </div>
 
         <!-- Other Filters -->
